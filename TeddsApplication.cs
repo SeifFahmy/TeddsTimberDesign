231833042da6c44a63f49cc1a7019161fc9d2594
Seif (Cundall) <seifehab.bl4@gmail.com>
2025-01-31 11:48:25 +0000

    implemented design logic to EC5

--- TeddsApplication.cs
+++ TeddsApplication.cs
@@ -1,15 +1,9 @@
 using System;
 using System.Diagnostics;
-using System.Windows;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Runtime.InteropServices;
 using Tekla.Structural.InteropAssemblies.TeddsCalc;
-using Tekla.Structural.InteropAssemblies.Tedds;
-
 
 
 namespace TeddsTimberDesign
@@ -18,7 +12,7 @@ namespace TeddsTimberDesign
     {
         #region Set-up
 
-        static readonly string calcFileName = "$(SysLbrDir)Timber beam design-BS5268-si-engb.lbr";
+        static readonly string calcFileName = "$(SysLbrDir)Timber member design-EN1995-si-engb.lbr";
         static readonly string calcItemName = "Timber member design";
         static readonly Calculator calculator = new Calculator();
 
@@ -70,39 +64,101 @@ namespace TeddsTimberDesign
         public static void ShowInitialWindow()
         {
             calculator.Initialize();
+            calculator.Functions.SetVar("N_{s1}", 1); // N = number of sections forming timber member
+            calculator.Functions.SetVar("_EnablePreview", 1);
             string defaultVariables = calculator.GetVariables();
             calculator.InitializeCalc(calcFileName, calcItemName, defaultVariables);
         }
         #endregion
 
         #region Member design
+
+        class SectionSize
+        {
+            public int Width { get; set; }
+            public int Depth { get; set; }
+        }
+
         /// <summary>
         /// This takes the previously user-defined material values, adds to them the member-specific variables, and runs the calculation. 
         /// </summary> 
-        public static Dictionary<string, object> DesignMember(List<MemberData> memberData)
+        public static List<Dictionary<string, object>> DesignMembers(List<MemberData> memberData)
         {
+            // TODO: let users know in front-end that spans over 6m are usually governed by vibration, not bending or shear.
+            var possibleSectionSizes = new List<SectionSize>() {
+                new SectionSize {Width=120, Depth=280},
+                new SectionSize {Width=160, Depth=360},
+                new SectionSize {Width=160, Depth=440},
+                new SectionSize {Width=200, Depth=520},
+                new SectionSize {Width=280, Depth=600},
+                new SectionSize {Width=320, Depth=680},
+                new SectionSize {Width=320, Depth=760},
+                new SectionSize {Width=320, Depth=840},
+                new SectionSize {Width=360, Depth=920},
+                new SectionSize {Width=360, Depth=1000},
+            };
+
             calculator.Functions.SetVar("_CalcUI", 0);
 
+            var results = new List<Dictionary<string, object>>();
             foreach (var member in memberData)
             {
-                // var id = member["id"];
-                // var moment = 
-                System.Console.WriteLine(member.Moment);
+                if (member.Axial >= 0) { calculator.Functions.SetVar("_AxialForce_{s1}", "Compression"); }
+                else { calculator.Functions.SetVar("_AxialForce_{s1}", "Tension"); }
+
+                calculator.Functions.SetVar("M_{y,d_s1}", member.MomentMajor, "kNm");
+                calculator.Functions.SetVar("M_{z,d_s1}", member.MomentMinor, "kNm");
+                calculator.Functions.SetVar("F_{y,d_s1}", Math.Abs(member.ShearMajor), "kN");
+                calculator.Functions.SetVar("F_{z,d_s1}", Math.Abs(member.ShearMinor), "kN");
+                calculator.Functions.SetVar("P_{d_s1}", Math.Abs(member.Axial), "kN");
+
+                string result = "fail";
+                double width, depth;
+                for (int i = 0; i < possibleSectionSizes.Count(); i++)
+                {
+                    var section = possibleSectionSizes[i];
+                    calculator.Functions.SetVar("b_{s1}", section.Width, "mm");
+                    calculator.Functions.SetVar("h_{s1}", section.Depth, "mm");
+
+                    string variables = calculator.GetVariables();
+                    calculator.InitializeCalc(calcFileName, calcItemName, variables);
+
+                    result = calculator.Functions.GetVar("_CalcResult").ToString().ToUpper();
+                    if (result == "PASS")
+                    {
+                        break;
+                    }
+
+                    double currentUtil = calculator.Functions.GetVar("_OverallUtilisation_{s1}").ToDouble();
+                    if (currentUtil > 2)
+                    {
+                        // given the section is far too small, skipping the next section saves time given it's likely also failing
+                        i = Math.Min(i + 1, possibleSectionSizes.Count() - 2);
+                    }
+                }
+
+                width = calculator.Functions.GetVar("b").ToDouble("mm");
+                depth = calculator.Functions.GetVar("h").ToDouble("mm");
+                double util = Math.Round(calculator.Functions.GetVar("_OverallUtilisation_{s1}").ToDouble(), 2);
+                string designMessage = calculator.Functions.GetVar("_OverallStatusMessage_{s1}").ToString();
+                string material = calculator.Functions.GetVar("MemberType").ToString();
+                string strengthClass = calculator.Functions.GetVar("StrengthClass").ToString();
+
+                var outputRtf = calculator.GetOutput();
+
+                results.Add(new Dictionary<string, object>(){
+                    { "id", member.Id },
+                    { "width", width },
+                    { "depth", depth },
+                    { "result", result },
+                    { "designMessage", designMessage },
+                    { "util", util },
+                    { "material", material },
+                    { "strength", strengthClass },
+                    { "outputRtf", outputRtf }
+                });
             }
-            calculator.Functions.SetVar("b", 600, "mm");
-            string variables = calculator.GetVariables();
 
-            calculator.InitializeCalc(calcFileName, calcItemName, variables);
-
-            //Query the calculation results
-            string width = calculator.Functions.GetVar("b").ToDouble("mm").ToString();
-            string moment = calculator.Functions.GetVar("M_{x}").ToDouble("kNm").ToString();
-
-            var results = new Dictionary<string, object>
-            {
-                { "width", width },
-                {"moment", moment}
-            };
             return results;
         }
         #endregion
